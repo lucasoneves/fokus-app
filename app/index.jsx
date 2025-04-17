0d4b4df2a5b531d9a2b7eceac37dd665b5a2c2ba
lucasoneves <casluhc@gmail.com>
2025-04-17 18:21:21 -0300

    Adding background

--- app/index.jsx
+++ app/index.jsx
@@ -2,20 +2,22 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={style.container}>
-      <Text style={style.text}>Fokus App</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Fokus App</Text>
     </View>
   );
 }
 
-const style = {
+const styles = {
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#021123",
   },
   text: {
     fontSize: 20,
     fontWeight: "bold",
+    color: "#fff",
   },
 };
